8a06a6a91b1cf5e60137cdae1286cf8e93048056
camilatigre <milatigre1@gmail.com>
2025-01-31 11:57:15 -0300

    change main.ts 3

--- backend-2/src/main.ts
+++ backend-2/src/main.ts
@@ -5,29 +5,25 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
-  try {
-    config(); // Carrega as variáveis de ambiente
-    logger.log('Variáveis de ambiente carregadas');
 
-    const app = await NestFactory.create(AppModule);
-    logger.log('Aplicação NestJS criada');
+  config(); // Carrega as variáveis de ambiente
+  logger.log('Variáveis de ambiente carregadas');
 
-    // Configuração do CORS
-    app.enableCors({
-      origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      credentials: true,
-    });
-    logger.log('CORS configurado');
+  const app = await NestFactory.create(AppModule);
+  logger.log('Aplicação NestJS criada');
 
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-    logger.log(`Aplicação rodando na porta ${port}`);
-  } catch (error) {
-    logger.error(`Erro ao iniciar a aplicação: ${error.message}`);
-    throw error;
-  }
+  // Configuração do CORS
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+  logger.log('CORS configurado');
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Aplicação rodando na porta ${port}`);
 }
 
 bootstrap();
